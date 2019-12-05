2245d45e6de0c2cbc90db14a213958b7879c5efe
Jefson Kris <39597167+kriskb24@users.noreply.github.com>
2019-12-05 15:08:17 -0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# kkkkkk
-Created with CodeSandbox
---
+++ src/views/home/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+
+</div>
+
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      info: 'hi there!'
+    };
+
+  }
+}; 
+</script>
+<style scoped>
+</style>
